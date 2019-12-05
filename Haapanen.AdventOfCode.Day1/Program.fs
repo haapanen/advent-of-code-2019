59e9c8d673dec504019cfda83a157f800beb1524
Jussi Haapanen <haapanen.jussi@gmail.com>
2019-12-05 18:02:29 +0200

    Add project files.

---
+++ Haapanen.AdventOfCode.Day1/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open System.IO
+
+let calculateRequiredFueld x = Math.Floor ((x / 3.0) - 2.0)
+
+[<EntryPoint>]
+let main argv =
+    let inputLines = File.ReadLines("Input.txt");
+
+    let modules = inputLines |> Seq.map int |> Seq.map float |> Seq.map calculateRequiredFueld 
+    
+    printfn "Required fuel: %d" (Seq.sum modules |> int)
+
+    0 // return an integer exit code
